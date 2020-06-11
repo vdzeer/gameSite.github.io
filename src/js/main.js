6520ace3f9e0fc1a0dd23595cb62deb3ca638f7f
Vadim <vdzerniuk@gmail.com>
2020-06-11 22:55:36 +0300

    Create slider

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,20 @@ const modal = document.querySelector('.modal');
 const closeModal = document.querySelector('.modal-text__close');
 const modalLinks = document.querySelector('.modal-links');
 
+const sliderArrow = document.querySelector('.slider-arrow');
+const slide1 =  document.querySelector('.item-1');
+const slide2 =  document.querySelector('.item-2');
+const slide3 =  document.querySelector('.item-3');
+let mass = [slide1, slide2, slide3];
+let act = 0;
+
+sliderArrow.addEventListener('click', function() {
+    mass[act].classList.add('disable');
+    act++;
+    if (act == 3) act = 0;
+    mass[act].classList.remove('disable');
+});
+
 navMenu.addEventListener('click', function() {
     modal.classList.remove('disable');
 });
@@ -16,3 +30,4 @@ closeModal.addEventListener('click', function() {
 modalLinks.addEventListener('click', function() {
     modal.classList.add('disable');
 });
+
