4d3aabdf25221c58fd677bf95c4a489d058fdbe1
Vadim <vdzerniuk@gmail.com>
2020-06-11 22:10:37 +0300

    Create modal interface

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const navMenu = document.querySelector('.navbar-menu');
+const modal = document.querySelector('.modal');
+const closeModal = document.querySelector('.modal-text__close');
+const modalLinks = document.querySelector('.modal-links');
+
+navMenu.addEventListener('click', function() {
+    modal.classList.remove('disable');
+});
+
+closeModal.addEventListener('click', function() {
+    modal.classList.add('disable');
+});
+
+modalLinks.addEventListener('click', function() {
+    modal.classList.add('disable');
+});
